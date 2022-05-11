29438807555412a6bf01ed2694133be1740d51dc
kumabio <kumabio.prince@student.moringaschool.com>
2022-05-11 18:47:05 +0000

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser ='Prince K';
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser+'!';
+
+//const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser +'!';
+
+const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}` + currentUser + "!";
+
+const shortGreeting = "Welcome, "+`${currentUser.slice(0,1)}` +'!';
